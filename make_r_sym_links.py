4dfff41e0b3e8b5d741e65b6f966abb238748033
David G <dgabbe@acm.org>
2016-04-24 20:46:49 -0400

    Remove unused code.

--- make_r_sym_links.py
+++ make_r_sym_links.py
@@ -15,17 +15,8 @@ config_files = [
 	{"home": os.path.join(home, ".Renviron"), "repo": "dot-Renviron"},
 	{"home": os.path.join(home, "Rprofile.site"), "repo": "Rprofile.site"},
 	{"home": os.path.join(home, "rconfig"), "repo": "rconfig.r"}
-#	(os.path.join(home, ".Renviron"), "dot-Renviron"),
-#	(os.path.join(home, "Rprofile.site"), "Rprofile.site"),
-#	(os.path.join(home, "rconfig"), "rconfig.r")
 	]
 	
-	
-# rEnviron = os.path.join(home, ".Renviron")
-# rProfileSite = os.path.join(home, "Rprofile.site")
-# rConfig = os.path.join(home, "rconfig")
-# config_files = (rEnviron, rProfileSite, rConfig)
-
 changesMade = False
 
 for f in config_files:
@@ -44,30 +35,3 @@ if changesMade:
 	print "    Then restart both R and RStudio and check for proper startup.\n\n"
 else:
 	print ""
-
-
-# if os.path.islink(rEnviron) and os.path.islink(rProfileSite) and os.path.islink(rconfig): 
-# 	print "\n    Sym links found. No changes to make.\n\n"
-# 	sys.exit()
-#	
-# if os.path.isfile(rEnviron):
-#  	os.rename(rEnviron, rEnviron.org)
-#  	print "\n    Moved ", rEnviron, " to ", rEnviron, ".org\n"
-# os.symlink(os.path.join(dir, "dot-Renviron"), rEnviron)
-#  
-# if os.path.isfile(rProfileSite):
-#  	os.rename(rProfileSite, rProfileSite.org)
-#  	print "\n    Moved ", rProfileSite, " to ", rProfileSite, ".org\n"
-# os.symlink(os.path.join(dir, "Rprofile.site"), rProfileSite)
-# 
-# print "\n    ", rEnviron, " -> ", os.path.realpath(rEnviron), "\n"
-# print "\n    ", rProfileSite, " -> ", os.path.realpath(rProfileSite), "\n"
-# print "\n    Please exit R and RStudio without saving the workspace.\n"
-# print "    Then restart both R and RStudio and check for proper startup.\n\n"
-
-
-
-
-
-
-
